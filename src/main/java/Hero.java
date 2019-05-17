2c57b16ab77cc8ddd0fc7d11af2743698d35f5eb
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-17 17:12:38 +0300

    Add a test for correct instantiation

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,14 @@
 public class Hero {
+    private String mName;
+    private int mAge;
+    private String mPower;
+    private String mWeakness;
+
+
+    public Hero(String name, int age, String power, String weakness){
+        mName = name;
+        mAge = age;
+        mPower = power;
+        mWeakness = weakness;
+    }
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -2,5 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HeroTest {
-
+    @Test
+    public void Hero_instantiatesCorrectly_true() {
+        Hero hero = new Hero("",0,"","");
+        assertTrue(hero instanceof Hero);
+    }
 }
