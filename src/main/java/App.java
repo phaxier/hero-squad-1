9e2b1445189316d62bfdc05262fbfef5ea3c5904
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-18 23:40:24 +0300

    Add routes to the success page and back to the home page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,17 +13,18 @@ public class App {
 
         get("/",(request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("template","index.vtl");
+            model.put("hero", request.session().attribute("hero"));
+            model.put("template","templates/index.vtl");
             return new ModelAndView(model, layout);
         },new VelocityTemplateEngine());
 
-        post("/tasks",(request, response) -> {
+        post("/heroes",(request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             ArrayList<Hero> heroes = request.session().attribute("heroes");
             if (heroes==null){
                 heroes = new ArrayList<Hero>();
-                request.session().attribute("heroes", heroes);          }
-
+                request.session().attribute("heroes", heroes);
+            }
 
             String name = request.queryParams("name");
             String age = request.queryParams("age");
@@ -31,7 +32,7 @@ public class App {
             String weakness = request.queryParams("weakness");
 
             Hero hero = new Hero(name, age, power, weakness);
-            request.session().attribute("hero",hero);
+            heroes.add(hero);
 
             model.put("template", "templates/success.vtl");
 
