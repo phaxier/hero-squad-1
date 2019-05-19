81229e6e366503b85d17dcc3dbf7b4747cfd51e5
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-19 22:07:56 +0300

    Add a squads page to view all squads, a squad page to show the squad's details and their routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -53,8 +53,16 @@ public class App {
 
         get("/squads", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("categories", Squad.all());
+            model.put("squads", Squad.all());
             model.put("template", "templates/squads.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/squads/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+            model.put("squad", squad);
+            model.put("template", "templates/squad.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
