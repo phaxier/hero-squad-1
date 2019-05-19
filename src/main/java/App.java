b1c2b1e98df4c9627afc2a23674c5a68dbd77346
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-19 17:49:52 +0300

    Add route to all heroes page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -24,22 +24,22 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        get("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("heroes", Hero.all());
+            model.put("template", "templates/heroes.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
         post("/heroes",(request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            ArrayList<Hero> heroes = request.session().attribute("heroes");
-            if (heroes==null){
-                heroes = new ArrayList<Hero>();
-                request.session().attribute("heroes", heroes);
-            }
-
             String name = request.queryParams("name");
             String age = request.queryParams("age");
             String power = request.queryParams("power");
             String weakness = request.queryParams("weakness");
 
             Hero hero = new Hero(name, age, power, weakness);
-            heroes.add(hero);
-
             model.put("template", "templates/success.vtl");
 
             return new ModelAndView(model, layout);
