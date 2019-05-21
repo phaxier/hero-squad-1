a9c0030faf9def09b0012ca349bf15a49edfb905
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-05-21 09:58:47 +0300

    Add gradlewrapper.jar to .gitignore

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,9 +7,24 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+
     public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
 
         get("/",(request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
